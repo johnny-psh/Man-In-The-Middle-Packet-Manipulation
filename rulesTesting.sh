a7db13944a11ddbeec5abc35331e7a61c6c1b4d4
Johnny Pan <ultimatejp88@gmail.com>
2018-09-25 09:04:54 +0800

    Update rulesTesting.sh

--- rulesTesting.sh
+++ rulesTesting.sh
@@ -5,6 +5,12 @@ cat /proc/sys/net/ipv4/ip_forward
 
 iptables -A FORWARD -p tcp --dport 7777 -j NFQUEUE --queue-num 0
 
+# Please run each of the command below on two seperate terminal. Basically the commands below is to perform arp spoofing. 
+# If you have a script that can do the same, you can safely ignore the commands below.
+
+# arpspoof -i eth0 -t 192.168.1.2 192.168.1.3
+# arpspoof -i eth0 -t 192.168.1.3 192.168.1.2
+
 
 
 
